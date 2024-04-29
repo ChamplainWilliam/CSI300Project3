0a0a5b8cb393fad33c90342bb2e0d8c866073f06
jacky.zhang <jacky.zhang@mymailchamplain.edu>
2024-04-29 15:29:54 -0400

    worked on homepage

--- index.php
+++ index.php
@@ -7,9 +7,12 @@
 </head>
 <body>
     <?php
-
         echo "Welcome to twitter <br/>";
-
+        echo "Please enter the user ID <br/>";
     ?>
+    <tr>
+        	<td class="labels">user ID:</td>
+            <td class="data"><input name="efn" size="20" ></td>
+    </tr>
 </body>
 </html> 
