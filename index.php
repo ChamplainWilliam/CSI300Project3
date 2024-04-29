632f900b4bcb50d028490b894522fb55f1d657e5
jacky.zhang <jacky.zhang@mymailchamplain.edu>
2024-04-29 16:07:45 -0400

    homepage

--- index.php
+++ index.php
@@ -2,17 +2,24 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <meta name="viewport" content="width=device_width, initial-scale=1.0">
+    <title>Twitter Clone</title>
+
 </head>
 <body>
-    <?php
-        echo "Welcome to twitter <br/>";
-        echo "Please enter the user ID <br/>";
-    ?>
-    <tr>
-        	<td class="labels">user ID:</td>
-            <td class="data"><input name="efn" size="20" ></td>
-    </tr>
+    <h1>LOG IN TO TWITTER USING YOUR USER ID</h1>    
+    <form action="timeline.php" method="POST">
+        <select name="user_id">
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+            <option value="4">4</option>
+            <option value="5">5</option>
+            <option value="6">6</option>
+            <option value="7">7</option>
+            <option value="8">8</option>
+        </select>  
+        <input type="submit" name="submit" value="submit">
+    </form>
 </body>
-</html> 
+</html>
