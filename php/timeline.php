554ebeaca8e054d596798b8659e59ab7b8d5bc5f
Chen <william.chen@mymail.champlain.edu>
2024-04-29 16:10:29 -0400

    test

---
+++ php/timeline.php
@@ -0,0 +1,19 @@
+<?php
+require("../db_connect.php")
+$user_id = $_COOKIE['user_id'];
+
+?>
+
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Timeline</title>
+    <link href="style.css"/>
+    <meta charset="URF-8">
+</head>
+<body>
+    
+</body>
+
+</html>
